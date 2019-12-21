04bfb84656295e4d5c9bc45db32779af380ebe33
Marcin Pietraszek <m.pietraszek@gmail.com>
2019-12-21 10:52:19 +0100

    Refactored day21e1

--- src/aoc/day21/day21.go
+++ src/aoc/day21/day21.go
@@ -11,6 +11,38 @@ func main() {
 	Main(inputFilePath)
 }
 
+func Main(inputFilePath string) {
+	lines := aoc.Read(inputFilePath)
+	fmt.Println("Exercise 1:", exercise1(lines[0]))
+	fmt.Println("Exercise 2:", exercise2(lines[0]))
+}
+
+func exercise1(memory string) int {
+	cpu := intcode.NewIntcode(intcode.NewMemory(memory))
+	program := `NOT C T
+OR T J
+NOT A T
+OR T J
+AND D J
+WALK
+`
+	for _, x := range program {
+		cpu.AddInput(int(x))
+	}
+
+	output := intcode.OutputMode
+	for output == intcode.OutputMode {
+		v, o := cpu.Output()
+		output = o
+		r := rune(v)
+		if v > 127 {
+			return v
+		}
+		fmt.Printf("%c", r)
+	}
+	return -1
+}
+
 /*
 
 AND X Y sets Y to true if both X and Y are true;
@@ -44,10 +76,8 @@ J123J123
 
 
 */
-
-func Main(inputFilePath string) {
-	lines := aoc.Read(inputFilePath)
-	cpu := intcode.NewIntcode(intcode.NewMemory(lines[0]))
+func exercise2(memory string) int {
+	cpu := intcode.NewIntcode(intcode.NewMemory(memory))
 	program := `NOT C T
 OR T J
 NOT A T
@@ -64,7 +94,10 @@ WALK
 		v, o := cpu.Output()
 		output = o
 		r := rune(v)
-		fmt.Println(v)
+		if v > 127 {
+			return v
+		}
 		fmt.Printf("%c", r)
 	}
+	return -1
 }
