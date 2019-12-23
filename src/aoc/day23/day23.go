ed25ba73d49f7b0562f88983ea0815bec4414f62
Marcin Pietraszek <m.pietraszek@gmail.com>
2019-12-23 22:41:57 +0100

    Small changes to find race condition

--- src/aoc/day23/day23.go
+++ src/aoc/day23/day23.go
@@ -4,6 +4,7 @@ import (
 	"aoc"
 	"aoc/intcode"
 	"fmt"
+	"time"
 )
 
 func main() {
@@ -12,6 +13,7 @@ func main() {
 }
 
 type networkInput struct {
+	isIdle        bool
 	address       int
 	values        []int
 	inputPosition int
@@ -29,36 +31,68 @@ func (i *networkInput) AddPacket(x, y int) {
 
 func (i *networkInput) Get() int {
 	if i.inputPosition >= len(i.values) {
+		i.isIdle = true
 		return -1
 	}
+	i.isIdle = false
 	value := i.values[i.inputPosition]
-	fmt.Printf("IN %2d: %d\n", i.address, value)
 	i.inputPosition++
 	return value
 }
 
-func run(i int, cpu *intcode.Intcode, inputs map[int]*networkInput, finished chan bool) {
+type nat struct {
+	x, y     int
+	previous int
+	inputs   map[int]*networkInput
+}
+
+func (n *nat) monitor() {
+	first := true
+	for {
+		time.Sleep(10000)
+		idle := true
+		for _, input := range n.inputs {
+			idle = idle && input.isIdle
+		}
+
+		if idle && !first && n.x+n.y != 0 {
+			fmt.Printf("NAT: %d %d\n", n.x, n.y)
+			n.inputs[0].AddPacket(n.x, n.y)
+			if !first && n.y == n.previous {
+				fmt.Println(n.y)
+				panic("double!")
+			}
+			first = false
+			n.previous = n.y
+		}
+	}
+}
+
+func run(i int, cpu *intcode.Intcode, inputs map[int]*networkInput, finished chan bool, n *nat) {
 	for {
 		destination, m := cpu.Output()
 		if m == intcode.HaltMode {
-			fmt.Printf("%2d: STOP a %d\n", i, destination)
+			fmt.Printf("%3d: STOP a %d\n", i, destination)
 			break
 		}
 		x, m := cpu.Output()
 		if m == intcode.HaltMode {
-			fmt.Printf("%2d: STOP b %d\n", i, destination)
+			fmt.Printf("%3d: STOP b %d\n", i, destination)
 			break
 		}
 		y, m := cpu.Output()
 		if m == intcode.HaltMode {
-			fmt.Printf("%2d: STOP c %d\n", i, destination)
+			fmt.Printf("%3d: STOP c %d\n", i, destination)
 			break
 		}
-		fmt.Printf("%2d: > %2d (%6d %6d)\n", i, destination, x, y)
 		if destination == 255 {
-			panic("!")
+			fmt.Printf("%3d > NAT (%6d %6d)\n", i, x, y)
+			fmt.Println(n)
+			n.x, n.y = x, y
+		} else {
+			fmt.Printf("%2d: > %3d (%6d %6d)\n", i, destination, x, y)
+			inputs[destination].AddPacket(x, y)
 		}
-		inputs[destination].AddPacket(x, y)
 	}
 	finished <- true
 }
@@ -67,6 +101,7 @@ func Main(inputFilePath string) {
 	lines := aoc.Read(inputFilePath)
 	cpus := map[int]*intcode.Intcode{}
 	inputs := map[int]*networkInput{}
+	n := &nat{inputs: inputs}
 	for i := 0; i < 50; i++ {
 		inputs[i] = &networkInput{address: i}
 		inputs[i].Add(i)
@@ -76,11 +111,9 @@ func Main(inputFilePath string) {
 	finished := make(chan bool)
 
 	for i, cpu := range cpus {
-		fmt.Println("Running", i)
-		go run(i, cpu, inputs, finished)
+		go run(i, cpu, inputs, finished, n)
 	}
+	go n.monitor()
 
-	for _ = range cpus {
-		<-finished
-	}
+	<-finished
 }
