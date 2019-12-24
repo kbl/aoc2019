1439efdc442121cc5be68571d233f732a9033bd6
Marcin Pietraszek <m.pietraszek@gmail.com>
2019-12-24 23:13:51 +0100

    Using int as representation

--- src/aoc/day24/day24.go
+++ src/aoc/day24/day24.go
@@ -19,70 +19,68 @@ func main() {
 func Main(inputFilePath string) {
 	lines := aoc.Read(inputFilePath)
 	grid := parse(lines)
-	fmt.Printf("Read %d lines!\n", len(grid))
 	fmt.Println(first(grid))
 }
 
-func first(grid [][]bool) int {
+func first(grid int) int {
 	seen := map[int]bool{}
-	r := rating(grid)
-	for !seen[r] {
+	for !seen[grid] {
 		fmt.Println(str(grid))
 		fmt.Println()
-		seen[r] = true
+		seen[grid] = true
 		grid = evolve(grid)
-		r = rating(grid)
-
 	}
-	fmt.Println(str(grid))
-	fmt.Println()
-	return r
+	return int(grid)
 }
 
-func str(grid [][]bool) string {
+func bit(x, y int) int {
+	return 1 << (y*5 + x)
+}
+
+func str(grid int) string {
 	repr := []string{}
-	for _, row := range grid {
-		r := []rune{}
-		for _, tile := range row {
-			tileS := '.'
-			if tile == bug {
-				tileS = '#'
+	for y := 0; y < 5; y++ {
+		row := []rune{}
+		for x := 0; x < 5; x++ {
+			if grid&bit(x, y) > 0 {
+				row = append(row, '#')
+			} else {
+				row = append(row, '.')
 			}
-			r = append(r, tileS)
 		}
-		repr = append(repr, string(r))
+		repr = append(repr, string(row))
 	}
 	return strings.Join(repr, "\n")
 }
 
-func evolve(grid [][]bool) [][]bool {
-	next := [][]bool{}
-	for y, row := range grid {
-		nextRow := []bool{}
-		for x, tile := range row {
+func isBug(grid int, x, y int) bool {
+	return grid&bit(x, y) > 0
+}
+
+func evolve(grid int) int {
+	next := 0
+	for y := 0; y < 5; y++ {
+		for x := 0; x < 5; x++ {
 			hm := 0
-			if x-1 >= 0 && grid[y][x-1] == bug {
+			if x-1 >= 0 && isBug(grid, x-1, y) {
 				hm++
 			}
-			if x+1 < 5 && grid[y][x+1] == bug {
+			if x+1 < 5 && isBug(grid, x+1, y) {
 				hm++
 			}
-			if y-1 >= 0 && grid[y-1][x] == bug {
+			if y-1 >= 0 && isBug(grid, x, y-1) {
 				hm++
 			}
-			if y+1 < 5 && grid[y+1][x] == bug {
+			if y+1 < 5 && isBug(grid, x, y+1) {
 				hm++
 			}
-			nextTile := empty
-			if tile == bug && hm == 1 {
-				nextTile = bug
+			if isBug(grid, x, y) && hm == 1 {
+				next |= bit(x, y)
 			}
-			if tile == empty && (hm == 1 || hm == 2) {
-				nextTile = bug
+			if !isBug(grid, x, y) && (hm == 1 || hm == 2) {
+				next |= bit(x, y)
 			}
-			nextRow = append(nextRow, nextTile)
 		}
-		next = append(next, nextRow)
 	}
 	return next
 }
@@ -93,8 +91,18 @@ func evolve(grid [][]bool) [][]bool {
 // 03 13 23 33 43
 // 04 14 24 34 44
 //
-// y in [0, 4]
-// look += [level + 1]
+// xy
+//
+// y = 0
+//   look += [level - 1][1][2]
+// y = 4
+//   look += [level - 1][3][2]
+// x = 0
+//   look += [level - 1][2][1]
+// x = 4
+//   look += [level - 1][2][3]
+// x == 2 && y == 1
+//   look += [level + 1][0][0] += [level + 1][0][0]
 // 00
 // 10
 // 20
@@ -147,30 +155,14 @@ func evolveRecursive(grids map[int][][]bool) map[int][][]bool {
 	return nextGrids
 }
 
-func rating(grid [][]bool) int {
-	rating := 0
-	for y, row := range grid {
-		for x, tile := range row {
-			if tile == bug {
-				rating += 1 << (y*5 + x)
+func parse(lines []string) int {
+	var grid int = 0
+	for y, l := range lines {
+		for x, r := range l {
+			if r == '#' {
+				grid |= bit(x, y)
 			}
 		}
-	}
-	return rating
-}
-
-func parse(lines []string) [][]bool {
-	grid := [][]bool{}
-	for _, l := range lines {
-		row := []bool{}
-		for _, r := range l {
-			tileType := bug
-			if r == '.' {
-				tileType = empty
-			}
-			row = append(row, tileType)
-		}
-		grid = append(grid, row)
 	}
 	return grid
 }
